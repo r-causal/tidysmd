b0a0e7470f1882f21e33e6740d2979309b837490
Malcolm Barrett <malcolmbarrett@gmail.com>
2022-08-11 12:46:43 -0400

    add tests

---
+++ tests/testthat/test-geom_love.R
@@ -0,0 +1,39 @@
+expect_doppelganger <- function(...) {
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(...)
+}
+
+test_that("multiplication works", {
+  skip_if_not_installed("ggplot2")
+  library(ggplot2)
+  .smds <- tidy_smd(
+    nhefs_weights,
+    c(age, race, education),
+    .group = qsmk,
+    .wts = c(w_ate, w_att, w_ato)
+  )
+
+  p1 <- ggplot(
+    .smds,
+    aes(
+      x = abs(.data$smd),
+      y = .data$variable,
+      group = .data$weights,
+      color = .data$weights,
+      fill = .data$weights
+    )
+  ) +
+    geom_love()
+
+  p2 <- love_plot(.smds)
+
+  expect_doppelganger(
+    title = "geom_love() works",
+    fig = p1
+  )
+
+  expect_doppelganger(
+    title = "love_plot() works",
+    fig = p2
+  )
+})
